42504186058b2a04d7fc9ffce099e536084d0777
Antoine <antoinesune@gmail.com>
2021-06-04 10:44:50 +0200

    initial commit

---
+++ routes/auth.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+
+const salt = 10;
+
+router.post("/signin", (req, res, next) => {
+  const { email, password } = req.body;
+  User.findOne({ email })
+    .then((userDocument) => {
+      if (!userDocument) {
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
+
+      const isValidPassword = bcrypt.compareSync(
+        password,
+        userDocument.password
+      );
+      if (!isValidPassword) {
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
+
+      req.session.currentUser = {
+        role: "admin",
+        id: userDocument._id,
+      };
+
+      res.redirect("/api/auth/isLoggedIn");
+    })
+    .catch(next);
+});
+
+router.post("/signup", (req, res, next) => {
+  const { email, password, firstName, lastName } = req.body;
+
+  User.findOne({ email })
+    .then((userDocument) => {
+      if (userDocument) {
+        return res.status(400).json({ message: "Email already taken" });
+      }
+
+      const hashedPassword = bcrypt.hashSync(password, salt);
+      const newUser = { email, lastName, firstName, password: hashedPassword };
+
+      User.create(newUser)
+        .then((newUserDocument) => {
+          /* Login on signup */
+          req.session.currentUser = newUserDocument._id;
+          res.redirect("/api/auth/isLoggedIn");
+        })
+        .catch(next);
+    })
+    .catch(next);
+});
+
+router.get("/isLoggedIn", (req, res, next) => {
+  if (!req.session.currentUser)
+    return res.status(401).json({ message: "Unauthorized" });
+
+  const id = req.session.currentUser;
+
+  User.findById(id)
+    .select("-password")
+    .then((userDocument) => {
+      res.status(200).json(userDocument);
+    })
+    .catch(next);
+});
+
+router.get("/logout", (req, res, next) => {
+  req.session.destroy(function (error) {
+    if (error) next(error);
+    else res.status(200).json({ message: "Succesfully disconnected." });
+  });
+});
+
+module.exports = router;
