198836a5456b13845e82670d28335a2f619c996a
Antoine <antoinesune@gmail.com>
2021-06-10 16:28:36 +0200

    build and app js modified for deploy

--- app.js
+++ app.js
@@ -59,18 +59,18 @@ if (_DEV_MODE) {
   });
 }
 
-// app.use("/api/*", (req, res, next) => {
-//   const error = new Error("Ressource not found.");
-//   error.status = 404;
-//   next(error);
-// });
+app.use("/api/*", (req, res, next) => {
+  const error = new Error("Ressource not found.");
+  error.status = 404;
+  next(error);
+});
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use("*", (req, res, next) => {
-//     // If no routes match, send them the React HTML.
-//     res.sendFile(path.join(__dirname, "public/build/index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use("*", (req, res, next) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(path.join(__dirname, "public/build/index.html"));
+  });
+}
 
 // // Test to see if user is logged In before getting into any router.
 // app.use(function (req, res, next) {
